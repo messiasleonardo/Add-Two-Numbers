35923fdd572697fd0fde6902edc59a3ada71801d
Leonardo Alves <leonardo.mess@terra.com.br>
2022-06-17 15:36:59 -0300

    Add project files.

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AddTwoNumbers;
+
+Solution solution = new Solution();
+ListNode ListNode1 = new ListNode(1);
+ListNode ListNode2 = new ListNode(0);
+
+solution.AddTwoNumbers(ListNode1,ListNode2);
+
+
---
+++ AddTwoNumbers/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int load = 0, sum;
+            ListNode head = new ListNode(0);
+            ListNode ln1 = l1, ln2 = l2, node = head;
+
+            while (load != 0 || ln1 != null || ln2 != null)
+            {
+                sum = load; 
+
+                if (ln1 != null)
+                {
+                    sum += ln1.val;
+                    ln1 = ln1.next;
+                }
+                if (ln2 != null)
+                {
+                    sum += ln2.val;
+                    ln2 = ln2.next;
+                }
+
+                load = sum / 10;
+                node.next = new ListNode(sum % 10);
+                node = node.next;
+            }
+            return head.next;
+        }
+    }
+}
